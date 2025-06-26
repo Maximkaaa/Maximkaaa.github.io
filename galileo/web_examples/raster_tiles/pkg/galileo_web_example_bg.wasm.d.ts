0f0f066f4e3c0c82478978c97e916250f3281844
Maxim <maxim@gritsenko.biz>
2025-06-26 13:19:29 +0900

    Update Galileo examples

---
+++ galileo/web_examples/raster_tiles/pkg/galileo_web_example_bg.wasm.d.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const init_vt_worker: () => void;
+export const process_message: (a: any) => any;
+export const __wbg_tileindex_free: (a: number, b: number) => void;
+export const __wbg_get_tileindex_z: (a: number) => number;
+export const __wbg_set_tileindex_z: (a: number, b: number) => void;
+export const __wbg_get_tileindex_x: (a: number) => number;
+export const __wbg_set_tileindex_x: (a: number, b: number) => void;
+export const __wbg_get_tileindex_y: (a: number) => number;
+export const __wbg_set_tileindex_y: (a: number, b: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure259_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h042dbec870fb402f_multivalue_shim: (a: number, b: number) => [number, number];
+export const __externref_table_dealloc: (a: number) => void;
+export const closure2846_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
---
+++ galileo/web_examples/vector_tiles/pkg/galileo_web_example.d.ts
@@ -0,0 +1,66 @@
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	export function main(): void;
+	export function init_vt_worker(): void;
+	export function process_message(msg: any): any;
+	/**
+	 * Index of a tile.
+	 */
+	export class TileIndex {
+	  private constructor();
+	  free(): void;
+	  /**
+	   * Z index.
+	   */
+	  z: number;
+	  /**
+	   * X index.
+	   */
+	  x: number;
+	  /**
+	   * Y index.
+	   */
+	  y: number;
+	}
+	
+}
+
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+declare interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: () => void;
+  readonly init_vt_worker: () => void;
+  readonly process_message: (a: any) => any;
+  readonly __wbg_tileindex_free: (a: number, b: number) => void;
+  readonly __wbg_get_tileindex_z: (a: number) => number;
+  readonly __wbg_set_tileindex_z: (a: number, b: number) => void;
+  readonly __wbg_get_tileindex_x: (a: number) => number;
+  readonly __wbg_set_tileindex_x: (a: number, b: number) => void;
+  readonly __wbg_get_tileindex_y: (a: number) => number;
+  readonly __wbg_set_tileindex_y: (a: number, b: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_1: WebAssembly.Table;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly closure315_externref_shim: (a: number, b: number, c: any) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h042dbec870fb402f_multivalue_shim: (a: number, b: number) => [number, number];
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly closure587_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure2819_externref_shim: (a: number, b: number, c: any) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+declare function wasm_bindgen (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
